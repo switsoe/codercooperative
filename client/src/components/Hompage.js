b1ebf953af7faab10e68a9b20413f124925afe4e
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-07-06 10:26:50 -0600

    front end

---
+++ client/src/components/Hompage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Homepage = () => (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">&lt;coderCooperative /&gt;</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">&lt;home /&gt;<span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">&lt;developers /&gt;</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+);
+
+export default Homepage;
