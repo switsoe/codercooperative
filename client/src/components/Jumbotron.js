1671d71d019ced59839044f6061c5d22b9a7ede2
essdubb <switsoe@gmail.com>
2018-07-06 13:12:14 -0600

    style tweaks

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,9 +3,12 @@ import React from "react";
 const Jumbotron = () => (
     <div className="jumbotron">
         <h1 className="display-4">&lt;coderCooperative /&gt;</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        </div>
+        <p className="lead" id="tagline">Code For Experience == Experience Solutions</p>
+    </div>
 
         );
         
         export default Jumbotron;
+
+        // text for logo
+        // &lt;coderCooperative /&gt;
